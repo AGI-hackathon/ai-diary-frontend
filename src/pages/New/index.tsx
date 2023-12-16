cf7ac1def889ef1b8f334e7947e0b375d874bf2c
gitruili <woshilirui@hotmail.com>
2023-12-16 21:45:09 +0800

    update new to change color

--- src/pages/New/index.tsx
+++ src/pages/New/index.tsx
@@ -1,12 +1,51 @@
 import {useState, useEffect} from 'react';
 import {Input, Button} from 'antd';
-import {publishDiary} from '@/pages/Home/request';
+import {publishDiary, getEmotion} from '@/pages/Home/request';
+import { animated, config, useSpring } from '@react-spring/web';
+import styles from './styles.module.css';
 
 const {TextArea} = Input;
 
 const NewDiary = () => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
+  const [emotion, setEmotion] = useState('');
+  // const [background, setBackground] = useState('var(--default)'); // Default background color
+  const [emotionClass, setEmotionClass] = useState('defaultEmotion'); // Initial class
+
+  // Function to map emotion to a class name
+  const updateEmotionClass = (emotion) => {
+    switch (emotion) {
+      case 'happy':
+        return 'happyEmotion';
+      case 'sad':
+        return 'sadEmotion';
+      case 'cry':
+        return 'cryEmotion';
+      case 'high':
+        return 'highEmotion';
+      case 'low':
+        return 'lowEmotion';
+      case 'normal':
+        return 'normalEmotion';
+      default:
+        return 'defaultEmotion';
+    }
+  };
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      if (content) {
+        getEmotion(content).then((res) => {
+          setEmotion(res.data.mood);
+          setEmotionClass(updateEmotionClass(res.data.mood));
+          console.log(res.data.mood);
+        });
+      }
+    }, 1000);
+
+    return () => clearTimeout(timeoutId);
+  }, [content]);
 
   return (
     <div>
@@ -22,7 +61,9 @@ const NewDiary = () => {
       />
 
       <div>Content of your diary</div>
+
       <TextArea
+        className={styles[emotionClass]} // Apply the dynamic class
         placeholder="Write down your mood diary here..."
         value={content}
         onChange={e => {
