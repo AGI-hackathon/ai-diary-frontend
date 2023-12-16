99f5ed197a1a61870e6ce824e0dea0b11f4dcbaf
姬峥崎 <jizhengqistrongman@126.com>
2023-12-16 08:41:30 +0800

    publish diary

--- src/pages/Home/request.js
+++ src/pages/Home/request.js
@@ -2,3 +2,4 @@ import axios from 'axios';
 
 export const getList = () => axios.get('/diary/fetch');
 export const getEmotion = text => axios.get(`/diary/emotion?current_text=${encodeURIComponent(text)}`);
+export const publishDiary = params => axios.put(`/diary/upload`, params);
--- src/pages/New/index.tsx
+++ src/pages/New/index.tsx
@@ -1,6 +1,47 @@
+import {useState, useEffect} from 'react';
+import {Input, Button} from 'antd';
+import {publishDiary} from '@/pages/Home/request';
+
+const {TextArea} = Input;
+
 const NewDiary = () => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
   return (
-    <div>new diary</div>
+    <div>
+      <h1>Write a new diary here</h1>
+
+      <div>Title of your diary</div>
+      <Input
+        placeholder="Write down your mood diary here..."
+        value={title}
+        onChange={e => {
+          setTitle(e.target.value);
+        }}
+      />
+
+      <div>Content of your diary</div>
+      <TextArea
+        placeholder="Write down your mood diary here..."
+        value={content}
+        onChange={e => {
+          setContent(e.target.value);
+        }}
+        rows={24}
+      />
+
+      <div>
+        <Button
+          type="primary"
+          onClick={() => {
+            publishDiary({title, content});
+          }}
+        >
+          Publish
+        </Button>
+      </div>
+    </div>
   );
 };
 
