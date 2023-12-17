a0e91ebb2ad341e61e5a4ed48c689ab5e8d7ba8c
天才 <18320122+incomingflyingbrick@users.noreply.github.com>
2023-12-17 10:00:15 +0800

    fix color

--- src/pages/Home/request.js
+++ src/pages/Home/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-//const url = 'https://gg3mbenk6m.ap-northeast-1.awsapprunner.com';
-const url = 'http://127.0.0.1:8000';
+const url = 'https://gg3mbenk6m.ap-northeast-1.awsapprunner.com';
+//const url = 'http://127.0.0.1:8000';
 
 export const getList = () => axios.get(`${url}/diary/fetch`);
 export const getEmotion = text => axios.get(`${url}/diary/emotion?current_text=${encodeURIComponent(text)}`);
